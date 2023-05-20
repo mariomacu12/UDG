078930854b8844098533a74a5c89dd25e4108307
mariomacu12 <69535252+mariomacu12@users.noreply.github.com>
2023-05-20 17:43:12 -0600

    Update datos_usuario.php
    PDF jalando

--- json_activity/datos_usuario.php
+++ json_activity/datos_usuario.php
@@ -1,4 +1,6 @@
 <?php 
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
 // leemos el usuario guardado en el json
 $target_directory = $_SERVER['DOCUMENT_ROOT']."/usuarios/";
 $archivo_path = $target_directory.'usuarios.json';
@@ -11,6 +13,38 @@ $json_array = json_decode($json, true);
 $nombre = $json_array['usuario'];
 $email = $json_array['email'];
 $fecha_nacimiento = $json_array['fecha_nacimiento'];
+
+function crearPDF($nombre, $email, $fecha_nacimiento){
+    echo 'PDF';
+
+    $pdf = new TCPDF();
+    $pdf->AddPage();
+    // $pdf->Write(1, "Ejemplo documento PDF", '', false, 'C');
+    $pdf->Write(2, "Datos del Usuario", '', false, 'C', true);
+    $pdf->Write(4, "", '', false, 'C', true);
+    $pdf->Write(1, "Nombre: ".$nombre, '', false, 'L', true);
+    $pdf->Write(1, "E-mail: ".$email, '', false, 'L', true);
+    $pdf->Write(1, "Fecha de Nacimiento: ".$fecha_nacimiento, '', false, 'L', true);
+    $pdf->Close();
+    ob_end_clean();
+    $pdf->Output('Usuario.pdf','I');
+}
+function crearWord($nombre, $email, $fecha_nacimiento){
+    echo 'Word';
+}
+function crearExcel($nombre, $email, $fecha_nacimiento){
+    echo 'Excel';
+}
+
+if (isset($_POST['btnPDF'])) {
+    crearPDF($nombre, $email, $fecha_nacimiento);
+}
+if (isset($_POST['btnWord'])) {
+    crearWord($nombre, $email, $fecha_nacimiento);
+}
+if (isset($_POST['btnExcel'])) {
+    crearExcel($nombre, $email, $fecha_nacimiento);
+}
 
 // lo pongo abajo para leer las variables creadas aqui
 require './vistas/datos_usuario.vista.php';
