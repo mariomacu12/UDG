f72d69fcf24b26a19047be7d987a09649ef5a028
mariomacu12 <69535252+mariomacu12@users.noreply.github.com>
2023-05-20 15:25:20 -0600

    Create registro.vista.php
    formulario de registro

---
+++ json_activity/vistas/registro.vista.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>crea una cuenta</title>
+</head>
+<body>
+<div class="contenedor">
+    <h1 class="titulo">Registrate</h1>
+    
+    <hr class="border">
+
+    <form class="formulario" name="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        <div class="form-group">
+            <input class="usuario" type="text" name="usuario" placeholder="Nombre de usuario">
+        </div>
+
+        <div class="form-group">
+            <input class="email" type="email" name="email" placeholder="Correo Eléctronico">
+        </div>
+
+        <div class="form-group">
+            <input class="fecha_nacimiento" type="date" name="fecha_nacimiento" placeholder="Fecha de nacimiento">
+        </div>
+
+        <div class="form-group">
+            <input class="password" type="password" name="password" placeholder="Contraseña">
+        </div>
+        <input type="submit">Guardar</input>
+    </form>
+</div>
+</body>
+</html>
