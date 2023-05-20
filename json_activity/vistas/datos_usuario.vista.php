23bc545ae2fb810332c984672730df80926b952d
mariomacu12 <69535252+mariomacu12@users.noreply.github.com>
2023-05-20 17:15:16 -0600

    Update datos_usuario.vista.php
    Agregando botones para los formatos de office y pdf

--- json_activity/vistas/datos_usuario.vista.php
+++ json_activity/vistas/datos_usuario.vista.php
@@ -10,10 +10,25 @@
 <div class="contenedor">
     <h1 class="titulo">Datos desde el archivo JSON</h1>
     <hr class="border">
-
+    
     <div> Nombre: <?php echo $nombre; ?></div>
     <div> Email: <?php echo $email; ?></div>
     <div> Fecha de nacimiento: <?php echo $fecha_nacimiento; ?></div>
+    
+    <!-- botones para los formatos -->
+    <hr>
+    <div>
+        <h4>Guardar datos en otros formatos</h4>
+        <form method="post" action="">
+            <button type="submit" name="btnPDF">PDF</button>
+        </form>
+        <form method="post" action="">
+            <button type="submit" name="btnWord">Word</button>
+        </form>
+        <form method="post" action="">
+            <button type="submit" name="btnExcel">Excel</button>
+        </form>
+    </div>
 </div>
 </body>
 </html>
