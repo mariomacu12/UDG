f108aef954ef5ef54b0559d50f474cbfabaf0853
mariomacu12 <69535252+mariomacu12@users.noreply.github.com>
2023-05-20 16:20:18 -0600

    Create datos_usuario.vista.php
    Mostrando los datos que se leyeron del json

---
+++ json_activity/vistas/datos_usuario.vista.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>crea una cuenta</title>
+</head>
+<body>
+<div class="contenedor">
+    <h1 class="titulo">Datos desde el archivo JSON</h1>
+    <hr class="border">
+
+    <div> Nombre: <?php echo $nombre; ?></div>
+    <div> Email: <?php echo $email; ?></div>
+    <div> Fecha de nacimiento: <?php echo $fecha_nacimiento; ?></div>
+</div>
+</body>
+</html>
