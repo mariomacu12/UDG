5af97c79ed3d5f1cb73b684dbe31a78345a57336
mariomacu12 <69535252+mariomacu12@users.noreply.github.com>
2023-05-25 18:31:58 -0600

    Update inicio_sesion.vista.php
    removiendo valor default

--- Act._2.2/vistas/inicio_sesion.vista.php
+++ Act._2.2/vistas/inicio_sesion.vista.php
@@ -14,11 +14,11 @@
 
 		<form class="formulario" name="login" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 			<div class="form-group">
-			<input class="usuario" type="text" name="usuario" placeholder="Nombre" value="norma">
+			<input class="usuario" type="text" name="usuario" placeholder="Nombre">
 			</div>
 
 			<div class="form-group">
-			<input class="password" type="password" name="password" placeholder="Password" value="norma">
+			<input class="password" type="password" name="password" placeholder="Password">
 				<i class="submit-btn" onclick="login.submit()">BOTON AQUI</i>
 			</div>
 
